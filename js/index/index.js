8ae662a93d308b4ca5bae9cf7f6588c43adf1ff1
kwakjkh <1210kwakjkh@gmail.com>
2024-09-02 17:49:41 +0900

    메인화면 banner 추가 , 스크롤에 따른 header js 작업 중

--- js/index/index.js
+++ js/index/index.js
@@ -1,10 +1,26 @@
 window.addEventListener("load", function () {
-    const swReview = new Swiper(".sw-review", {
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        slidesPerView : 3,
-        spaceBetween : 40
-      });
+  const swBanner = new Swiper(".sw-banner", {});
+  const swReview = new Swiper(".sw-review", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+  });
+  const header = this.document.querySelector(".header");
+  this.window.addEventListener("scroll", function () {
+    scy = this.window.document.documentElement.scrollTop;
+    if (scy > 920) {
+      header.classList.remove("hide");
+      header.classList.add("active");
+    }else if (scy > 110) {
+      header.classList.add("hide");
+      header.classList.remove("active");
+    } else {
+      header.classList.remove("hide");
+      header.classList.remove("active");
+    }
+  });
 });
