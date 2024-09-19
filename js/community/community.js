882ebf42adb733f257ccdd6b508f404ef14d5f6b
seonmin <seonmin1414@gmail.com>
2024-09-19 15:59:47 +0900

    커뮤니티 수정 및 js 추가 (전체적인 레이아웃및 css을 수정함)

--- js/community/community.js
+++ js/community/community.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", function () {
-    
-})
+  const noticeBtn = this.document.querySelector(".community-notice-title");
+  const noticeSection = this.document.querySelector(".notice-list");
+  const qnaBtn = this.document.querySelector(".community-qAa-title");
+  const qnaSection = this.document.querySelector(".qAa-list");
+  noticeBtn.addEventListener("click", function () {
+    noticeBtn.classList.add("active");
+    qnaBtn.classList.remove("active");
+    noticeSection.style.display = "block";
+    qnaSection.style.display = "none";
+  });
+  qnaBtn.addEventListener("click", function () {
+    qnaBtn.classList.add("active");
+    noticeBtn.classList.remove("active");
+    qnaSection.style.display = "block";
+    noticeSection.style.display = "none";
+  });
+});
