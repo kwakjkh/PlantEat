eee1f8d324ed88a5aa1e08bd854b28b7a368c216
seonmin <seonmin1414@gmail.com>
2024-09-25 14:22:08 +0900

    잇츠 베러 , 실크 어바웃 세션 버튼 크기 및 css 통일, 상품 페이지 반응형 최종 완료, 상품 페이지 가격 합계 오류 수정

--- js/product-page/product-page.js
+++ js/product-page/product-page.js
@@ -31,12 +31,27 @@ function displayPpNum() {
 
 // 쿠폰 할인 적용 함수
 function couponDiscount(coupon) {
+  const discountedPrice = ppPrice + d_coupon; // 현재 총 가격
+  const couponBtn = document.querySelector(".first_coupon"); // 쿠폰 버튼 선택
+
+  // 총 가격이 35,000원을 초과하면 쿠폰 할인 적용 불가
+  if (discountedPrice > 35000) {
+    alert("총 가격이 35,000원을 초과하여 쿠폰 할인이 적용되지 않습니다.");
+    couponBtn.classList.add("false"); // 버튼 색상 변경 (비활성화 상태)
+    return; // 함수 실행 중지
+  } else {
+    couponBtn.classList.remove("false"); // 버튼 색상 변경 (비활성화 상태)
+  }
+
+  couponBtn.classList.toggle("active"); // 버튼 색상 변경 (비활성화 상태)
+
   if (d_coupon < 3500) {
     // 최대 3500원까지 할인 가능
     d_coupon += coupon;
   } else {
     d_coupon -= coupon;
   }
+
   displayCoupon();
   displayTotal();
 }
@@ -51,14 +66,17 @@ function displayCoupon() {
 function displayTotal() {
   const total_price = document.querySelector(".total_price");
   const overcoupon = document.querySelector("#coupon-discount");
+  const first_coupon = document.querySelector(".first_coupon");
   const common_price = ppPrice;
   const discountedPrice = ppPrice + d_coupon; // 쿠폰 할인을 적용한 총 가격
   const over35000 = d_coupon - 3500;
+  const notover35000 = d_coupon;
   if (discountedPrice > 35000) {
     total_price.innerHTML = `${common_price.toLocaleString()}`;
-    overcoupon.innerHTML = `${over35000.toLocaleString()}`;
+    overcoupon.innerHTML = `0`;
   } else {
     total_price.innerHTML = `${discountedPrice.toLocaleString()}`;
+    overcoupon.innerHTML = `${notover35000.toLocaleString()}`;
   }
 }
 
@@ -75,6 +93,13 @@ window.addEventListener("load", function () {
       mainImg.alt = img.alt;
     });
   });
+
+  const payNow = this.document.querySelector(".payNow");
+  payNow.addEventListener("click", function () {
+    alert("로그인 후 이용 가능합니다.")
+    window.location.href = "login.html"; // 알림창 확인 후 login.html로 이동
+  });
+
   // 리뷰, 문의 더보기 누를 시 나타나는 알림창
   const prevbtn = this.document.querySelectorAll(".prev");
   const nextbtn = this.document.querySelectorAll(".next");
@@ -110,6 +135,7 @@ window.addEventListener("load", function () {
     });
   });
 
+  // 문의 더보기
   const ask_pages = this.document.querySelectorAll(".qna-list");
   const ask_num_btn = this.document.querySelectorAll(".other_ask .num-btn button");
 
---
+++ js/product-page/product-page_jq.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".pay-p-info h1").on("click", function(){
+        $(".pay-p-info .p-wrap").slideToggle(200)
+    })
+    $(".delivery-info h1").on("click", function(){
+        $(".delivery-info .p-wrap").slideToggle(200)
+    })
+    $(".return-policy-info h1").on("click", function(){
+        $(".return-policy-info .p-wrap").slideToggle(300)
+    })
+})
